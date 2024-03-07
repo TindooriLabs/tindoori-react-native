7fba57b13dfab3ebd08ebe55eb4cfa8cdee8ea4e
tindoori-dragon300 <project.tindoori@gmail.com>
2024-03-07 14:09:20 -0500

    Added name input screen

--- src/pages/NameInput/index.tsx
+++ src/pages/NameInput/index.tsx
@@ -1,4 +1,4 @@
-import { View, Text } from "react-native";
+import { View, Text, Alert } from "react-native";
 import S from "./styles";
 import { Title } from "components/Title";
 import { widthPercentageToDP as wp } from "react-native-responsive-screen";
@@ -10,6 +10,8 @@ import { errorHandler } from "api/errorHandler";
 import { useEffect, useState } from "react";
 import { ModalActivityIndicator } from "components/ModalActivityIndicator";
 import { FieldInput } from "components/FieldInput";
+import { getFirstName, updateFirstName } from "api/profile";
+import axios from "axios";
 
 export const NameInput = ({ navigation }: RegisterNavigatorProps) => {
   const { t } = useTranslation();
@@ -27,38 +29,47 @@ export const NameInput = ({ navigation }: RegisterNavigatorProps) => {
   const [loading, setLoading] = useState(false);
 
   useEffect(() => {
-    // setLoading(true);
-    // getMobile()
-    //   .then((response) => {
-    // if (response.data.mobile && response.data.mobile.length === 11) {
-    //   setValue("phoneNumber", formatPhoneNumber(response.data.mobile.substring(1)), {
-    //     shouldValidate: true,
-    //     shouldDirty: true,
-    //   });
-    // }
-    // setLoading(false);
-    //   })
-    //   .catch((e) => {
-    //     let errorData = errorHandler(e);
-    //     Alert.alert("Error", errorData.message);
-    //     setLoading(false);
-    //   });
+    setLoading(true);
+    getFirstName()
+      .then((response) => {
+        if (response.data.firstName) {
+          setValue("firstName", response.data.firstName, {
+            shouldValidate: true,
+            shouldDirty: true,
+          });
+        }
+        setLoading(false);
+      })
+      .catch((e) => {
+        if (axios.isAxiosError(e)) {
+          let errorCode = e.response ? e.response.status : e.code;
+          if (errorCode !== 404) {
+            let errorData = errorHandler(e);
+            Alert.alert("Error", errorData.message);
+          }
+        } else {
+          let errorData = errorHandler(e);
+          Alert.alert("Error", errorData.message);
+        }
+
+        setLoading(false);
+      });
   }, []);
 
   const onSubmit = async (data: any) => {
-    // setLoading(true);
-    // updateMobile({
-    //   mobile: "1" + data.phoneNumber.replace(/\D/g, ''),
-    // })
-    //   .then((response) => {
-    //     setLoading(false);
-    //     navigation.navigate("PhoneNumberVerification");
-    //   })
-    //   .catch((error) => {
-    //     setLoading(false);
-    //     let errorData = errorHandler(error);
-    //     Alert.alert("Error", errorData.message);
-    //   });
+    setLoading(true);
+    updateFirstName({
+      firstName: data.firstName,
+    })
+      .then((response) => {
+        setLoading(false);
+        // navigation.navigate("PhoneNumberVerification");
+      })
+      .catch((error) => {
+        setLoading(false);
+        let errorData = errorHandler(error);
+        Alert.alert("Error", errorData.message);
+      });
   };
 
   return (
@@ -69,12 +80,12 @@ export const NameInput = ({ navigation }: RegisterNavigatorProps) => {
         size={wp("25%")}
         showText={false}
       />
-      <Title titleText="What's your name?" />
+      <Title titleText={t("NameInput.title")} />
       <Controller
         control={control}
         rules={{
           required: true,
-          pattern: /\S+@\S+\.\S+/,
+          maxLength: 100,
         }}
         render={({ field: { onChange, onBlur, value } }) => (
           <FieldInput
@@ -86,55 +97,14 @@ export const NameInput = ({ navigation }: RegisterNavigatorProps) => {
             }}
             value={value}
             onChange={onChange}
-            label={"First Name"}
+            label={t("NameInput.firstNameLabel")}
             isSecure={false}
             marginTop="4.27%"
           />
         )}
         name="firstName"
       />
-      <Text style={S.verificationText}>
-        {
-          "This will appear on your profile, and you won’t be able to change it later."
-        }
-      </Text>
-      {/* <View style={S.phoneNumberInputView}>
-        <Image
-          source={require("../../../assets/usaFlag.png")}
-          style={S.usaFlag}
-        />
-        <Text style={S.usText}>{t("PhoneNumberInput.country")}</Text>
-        <Text style={S.countryCode}>+1</Text>
-        <Controller
-          control={control}
-          rules={{
-            required: true,
-            minLength: 14,
-            maxLength: 14,
-          }}
-          render={({ field: { onChange, onBlur, value } }) => (
-            <TextInput
-              onBlur={onBlur}
-              onChangeText={(text) => {
-                // onChange(text.replace(/[^0-9]/g, ""));
-                onChange(formatPhoneNumber(text))
-              }}
-              value={value}
-              style={S.phoneNumberText}
-              keyboardType="phone-pad"
-              placeholderTextColor="rgba(113, 128, 150, 0.7)"
-              maxLength={14}
-            />
-          )}
-          name="phoneNumber"
-        />
-      </View>
-      <View style={S.horizontalRuleView}>
-        <View style={S.horizontalRule} />
-      </View>
-      <Text style={S.verificationText}>
-        {t("PhoneNumberInput.verificationText")}
-      </Text> */}
+      <Text style={S.verificationText}>{t("NameInput.verificationText")}</Text>
       <ContinueButton
         isBottom={true}
         buttonText={t("Shared.conitnueButton")}
